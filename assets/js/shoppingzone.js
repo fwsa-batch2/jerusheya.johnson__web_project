9fb86152942f8d7b2e2c7e5f4f87b1fa89c363f8
Jerusheya <jerusheya2002@gmail.com>
2021-12-22 00:41:40 +0530

    added pages to shopping zone

---
+++ assets/js/shoppingzone.js
@@ -0,0 +1,12 @@
+function category(){
+    window.location.href="../pages/category.html"
+}
+function wallets(){
+    window.location.href="../pages/wallets.html"
+}
+function hairband(){
+    window.location.href="../pages/hairAccessories.html"
+}
+function lunchbox(){
+    window.location.href="../pages/lunchbox.html"
+}
