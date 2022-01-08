a4737bf60eda92ae0ff366ffb6a07ea26b7c7633
Jerusheya <jerusheya2002@gmail.com>
2022-01-08 18:58:44 +0530

    added features

---
+++ assets/js/payment.js
@@ -0,0 +1,36 @@
+let paymentdetails = [];
+
+function onPageLoad() {
+  console.group("onPageLoad");
+  let users = JSON.parse(localStorage.getItem('payment'));
+  console.log(users)
+  if (users == null){
+    paymentdetails = []
+  }else {
+    paymentdetails = users;
+  }
+  console.groupEnd("onPageLoad")
+}
+
+function submitHandlers() {
+  event.preventDefault();
+  console.group("submitHandlers");
+  let cardholder = document.getElementById("cardholder").value;
+  let cardno = document.getElementById("cardno").value;
+  let exdate = document.getElementById("exdate").value;
+  let cvv = document.getElementById("cvv").value;
+
+  let customerDetail = {
+    "cardholder": cardholder,
+    "cardno": cardno,
+    "exdate": exdate,
+    "cvv": cvv
+  }
+    console.table(customerDetail);
+    paymentdetails.push(customerDetail);
+    let pay = JSON.stringify(paymentdetails);
+    localStorage.setItem("payment", pay);
+    window.location.href = "../pages/thankyou disney.html";
+    console.groupEnd("submitHandlers") ;
+}
+onPageLoad();
--- assets/js/ticket booking.js
+++ assets/js/ticket booking.js
@@ -7,52 +7,9 @@ function onPageLoad(){
         ticketbooking = users;
       }
   }
+ 
+  function ticket(){
   
-  function submitHandlers() {
-      event.preventDefault();
-      console.group("submitHandler");
-      let  name = document.getElementById("name").value;
-      let  number= document.getElementById("number").value;
-      let  emailid= document.getElementById("email id").value;
-      let  lastname= document.getElementById("lastname").value;
-      let  phonenumber= document.getElementById("phone number").value;
-      let  date= document.getElementById("date").value;
-      let  allergies= document.getElementById("input1").value;
-      let  yesSpecialSeat= document.getElementById("yesSpecialSeat").value;
-      let  noSpecialSeat= document.getElementById("noSpecialSeat").value;
-      let  yesHandicap= document.getElementById("yesHandicap").value;
-      let  noHandicap= document.getElementById("noHandicap").value;
-  
-         
-     
-      let ticketdetails = {
-          "name": name,
-          "number": number,
-          "emailid":emailid,
-          "lastname":lastname,
-          "phonenumber":phonenumber,
-          "date":date,
-          "allergies":allergies,
-          "yesSpecialSeat":yesSpecialSeat,
-          "noSpecialSeat":noSpecialSeat,
-          "yesHandicap":yesHandicap,
-          "noHandicap":noHandicap
-      }
-     
-      console.table(ticketdetails);
-      ticketbooking.push(ticketdetails);
-      localStorage.setItem("ticketdetails", JSON.stringify(ticketbooking));
-      console.groupEnd("submitHandler");
-      sendEmail();
-      window.location.href = "../pages/thankyou disney.html";
-
-
-  }
-  
-  
-  onPageLoad();
-
- function ticket(){
   let  name = document.getElementById("name").value;
   let  number= document.getElementById("number").value;
   let  emailid= document.getElementById("email id").value;
@@ -85,7 +42,10 @@ return passengerTicket;
   
 function sendEmail() {
   event.preventDefault();
-  let  email= document.getElementById("email id").value.toLowerCase();
+ 
+
+
+ let  email= document.getElementById("email id").value.toLowerCase();
   Email.send({
        Host : "smtp.gmail.com",
        Username : "disneyholidayhype@gmail.com",
@@ -97,7 +57,45 @@ function sendEmail() {
    }).then(
      message => alert('Mail Delivered successfully!')
    );
+   
+     
 }
   
 
   
+function StorageOfdetails(){
+
+   event.preventDefault();
+
+  let  name = document.getElementById("name").value;
+  let  number= document.getElementById("number").value;
+  let  emailid= document.getElementById("email id").value;
+  let  lastname= document.getElementById("lastname").value;
+  let  phonenumber= document.getElementById("phone number").value;
+  let  date= document.getElementById("date").value;
+  let  allergies= document.getElementById("input1").value;
+  let  yesSpecialSeat= document.getElementById("yesSpecialSeat").value;
+  let  noSpecialSeat= document.getElementById("noSpecialSeat").value;
+  let  yesHandicap= document.getElementById("yesHandicap").value;
+  let  noHandicap= document.getElementById("noHandicap").value;
+
+ let passengerTicket={
+    "name": name,
+    "number": number,
+    "emailid":emailid,
+    "lastname":lastname,
+    "phonenumber":phonenumber,
+    "date":date,
+    "allergies":allergies,
+    "yesSpecialSeat":yesSpecialSeat,
+    "noSpecialSeat":noSpecialSeat,
+    "yesHandicap":yesHandicap,
+    "noHandicap":noHandicap
+}
+  console.table(passengerTicket);
+  ticketbooking.push(passengerTicket);
+  localStorage.setItem("ticketdetails", JSON.stringify(ticketbooking));
+  window.location.href = "../pages/payment_page.html";
+}
+
+onPageLoad();
